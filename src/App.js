2755e063884c8d5bc51188556c4e5b321e3d0459
Eden <edenn.m@gmail.com>
2021-08-19 12:52:20 +0300

    auth ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React from "react";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+import { Route, Switch } from "react-router-dom";
 
 export default function App() {
-  return <div></div>;
+  return (
+    <>
+      <Switch>
+        <Route exact path={["/", "/signin"]}>
+          <SignIn />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+      </Switch>
+    </>
+  );
 }
-
--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,34 +1,34 @@
-import { createTheme } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
+import { createTheme } from "@material-ui/core/styles";
+import { CssBaseline } from "@material-ui/core";
 
 const lightPalette = {
-  main: '#725cff',
-  background: '#FFFFFF',
-  middle: '#EDEBFF',
-  semi: '#C2B8FF',
-  contrastText: '#090023',
+  main: "#725cff",
+  background: "#FFFFFF",
+  middle: "#EDEBFF",
+  semi: "#C2B8FF",
+  contrastText: "#090023",
 };
 
 const darkPalette = {
-  main: '#725cff',
-  background: '#090023',
-  middle: '#240066',
-  semi: '#72659D',
-  contrastText: '#FFFFFF',
+  main: "#725cff",
+  background: "#090023",
+  middle: "#240066",
+  semi: "#72659D",
+  contrastText: "#FFFFFF",
 };
 
 export const theme = (mode) =>
   createTheme({
     ...CssBaseline,
     palette: {
-      type: mode === 'light' ? 'light' : 'dark',
-      primary: mode === 'light' ? lightPalette : darkPalette,
+      type: mode === "light" ? "light" : "dark",
+      primary: mode === "light" ? lightPalette : darkPalette,
       secondary: {
-        main: '#EE7674',
+        main: "#EE7674",
       },
     },
     typography: {
-      fontFamily: 'Inter',
+      fontFamily: "Inter",
     },
     shape: {
       borderRadius: 4,
@@ -36,21 +36,21 @@ export const theme = (mode) =>
     overrides: {
       MuiButton: {
         root: {
-          textTransform: 'none',
+          textTransform: "none",
         },
       },
     },
     props: {
       MuiButton: {
         disableRipple: true,
-        variant: 'contained',
-        color: 'primary',
+        variant: "contained",
+        color: "primary",
       },
       MuiCheckbox: {
         disableRipple: true,
       },
       MuiTextField: {
-        variant: 'outlined',
+        variant: "outlined",
         InputLabelProps: {
           shrink: true,
         },
